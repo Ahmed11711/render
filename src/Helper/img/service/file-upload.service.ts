1cfac3bd1755d6631e358d198b0a6bf0441edf1c
Ahmed Samir <134524061+Ahmed11711@users.noreply.github.com>
2025-01-15 03:36:04 +0200

    Update file-upload.service.ts

--- src/Helper/img/service/file-upload.service.ts
+++ src/Helper/img/service/file-upload.service.ts
@@ -6,6 +6,34 @@ import * as path from 'path';
 
 @Injectable()
 export class FileService {
-  
+  public s3Client: S3Client;
+  public bucketName: string;
+
+  constructor() {
+    this.s3Client = new S3Client({
+      region: 'eu-central-1',
+      credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey:process.env.AWS_SECRET_ACCESS_KEY,
+      },
+    });
+
+    this.bucketName =process.env.AWS_SECRET_ACCESS_KEY;
+  }
+
+  async uploadFile(file: Express.Multer.File,folderName)  {
+    const extension = path.extname(file.originalname); 
+    const fileKey = `${Date.now()}${extension}`;
+    const params = {
+        Bucket: this.bucketName,
+        Key: `${folderName}/${fileKey}`,  
+        Body: file.buffer,
+        ContentType: file.mimetype,
+      };
+ 
+    await this.s3Client.send(new PutObjectCommand(params));
+    // return `https://${this.bucketName}.s3.amazonaws.com/${folderName}/${fileKey}`;
+    return `${folderName}/${fileKey}`;
+}
 }
  
